dc9f6d8cc6ebae7b7f83b72ff60268def43fd10a
Исламов Руслан <rislamov@systematic.ru>
2017-06-30 14:49:22 +0300

    refactoring

--- app/src/main/java/com/example/pyc/myapplication/fragments/Settings.java
+++ app/src/main/java/com/example/pyc/myapplication/fragments/Settings.java
@@ -35,7 +35,6 @@ public class Settings extends PreferenceFragment {
         //кнопка Импорта записей
         Preference buttonImport = findPreference(getString(R.string.importDB));
         buttonImport.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-
             //переменная для проверки двойного нажатия
             private  boolean doubleTapPressedOnce=false;
 
@@ -67,10 +66,29 @@ public class Settings extends PreferenceFragment {
         //кнопка Экспорта записей
         Preference buttonExport = findPreference(getString(R.string.exportDB));
         buttonExport.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            //переменная для проверки двойного нажатия
+            private  boolean doubleTapPressedOnce=false;
+
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 //ссылка на меотод с контекстом
-                BackupAndRestore.exportDB(getActivity());
+                //BackupAndRestore.exportDB(getActivity());
+                //return true;
+                if (!doubleTapPressedOnce) {
+                    this.doubleTapPressedOnce = true;
+                    Toast.makeText(getActivity(), R.string.warning_export_all, Toast.LENGTH_SHORT).show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            doubleTapPressedOnce = false;
+                        }
+                    }, 2500);
+
+                } else {
+                    //ссылка на меотод с контекстом
+                    BackupAndRestore.exportDB(getActivity());
+
+                }
                 return true;
             }
         });
